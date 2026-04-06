8c772eaf32ef27a88de79468e1f5a85b73d122e9
HaoqiangLu <luhaoqiang0513@qq.com>
2026-04-06 19:52:02 +0800

    tile: improve bn mul Change: add bn mul low noraml Type: feat Reason: improving bn mul Impact: No impact Notes: None

--- lib/bn_mul.c
+++ lib/bn_mul.c
@@ -61,3 +61,26 @@ void bn_mul_normal(BN_TYPE_ULONG *r,
         b  += 4;
     }
 }
+
+void bn_mul_low_normal(BN_TYPE_ULONG *r, BN_TYPE_ULONG *a, BN_TYPE_ULONG *b, int n)
+{
+    bn_mul_words(r, a, b, b[0]);
+
+    for (;;)
+    {
+        if (--n <= 0) return;
+        bn_mul_add_words(&(r[1]), a, n, b[1]);
+
+        if (--n <= 0) return;
+        bn_mul_add_words(&(r[2]), a, n, b[2]);
+
+        if (--n <= 0) return;
+        bn_mul_add_words(&(r[3]), a, n, b[3]);
+
+        if (--n <= 0) return;
+        bn_mul_add_words(&(r[4]), a, n, b[4]);
+
+        r += 4;
+        b += 4;
+    }
+}
