1daf604e34f404ea0a1530ce5f1ef5ccfa60c452
HaoqiangLu <luhaoqiang0513@qq.com>
2026-04-11 00:13:47 +0800

    Tile: improve bn_mul Change: improve bn_mul_part_recursive Reason: add bn_mul Type: feat Impact: No impact Notes: None

--- lib/bn_mul.c
+++ lib/bn_mul.c
@@ -274,7 +274,11 @@ void bn_mul_recursive(BN_TYPE_ULONG *r,
      */
     case -2:
         bn_sub_part_words(t, &a[n], a, tna, tna - n);       // A1 减 A0
+<<<<<<< Updated upstream
         bn_sub_part_words(&t[n], b, &b[n], tnb, n - tnb);   // B1 减 B0
+=======
+        bn_sub_part_words(&t[n], &b[n], b, tnb, tnb - n);   // B1 减 B0
+>>>>>>> Stashed changes
         neg = 1;    // 标记结果为负数
         break;
     /*
@@ -442,9 +446,136 @@ void bn_mul_part_recursive(BN_TYPE_ULONG *r,
         return;
     }
 
+<<<<<<< Updated upstream
     /* r = (a[0]-a[1])*(b[1]-b[0]) */
     c1 = bn_cmp_part_words(a, &a[n], tna, n - tna);
     c2 = bn_cmp_part_words(&b[n], b, tnb, tnb - n);
+=======
+    /* 此处同bn_mul_recursive()的过程 */
+    c1 = bn_cmp_part_words(a, &a[n], tna, n - tna);
+    c2 = bn_cmp_part_words(&b[n], b, tnb, tnb - n);
+    neg = 0;
+    switch (c1 * 3 + c2)
+    {
+    case -4:
+        bn_sub_part_words(t, &a[n], a, tna, tna - n);
+        bn_sub_part_words(&t[n], b, &b[n], tnb, n - tnb);
+        break;
+    case -3:    /* fall through */
+    case -2:
+        bn_sub_part_words(t, &a[n], a, tna, tna - n);
+        bn_sub_part_words(&t[n], &b[n], b, tnb, tnb - n);
+        neg = 1;
+        break;
+    case -1:    /* fall through */
+    case 0:     /* fall through */
+    case 1:     /* fall through */
+    case 2:
+        bn_sub_part_words(t, a, &a[n], tna, n - tna);
+        bn_sub_part_words(&t[n], b, &b[n], tnb, n - tnb);
+        neg = 1;
+        break;
+    case 3:     /* fall through */
+    case 4:
+        bn_sub_part_words(t, a, &a[n], tna, n - tna);
+        bn_sub_part_words(&t[n], &b[n], b, tnb, tnb - n);
+        break;
+    }
+    /*
+     * 0 的情况（差值为 0）在这里还没实现。但就算实现了，速度提升也几乎可以忽略。
+     */
+
+    p = &t[n2 * 2];
+    // 算 P2 = (a0-a1)(b1-b0) → t[n2 ... 2·n2-1]
+    bn_mul_recursive(&t[n2], t, &t[n], n, 0, 0, p);
+    // 算 P0 = a0*b0 → r[0 ... n2-1]
+    bn_mul_recursive(r, a, b, n, 0, 0, p);
+    i = n / 2;
+    /*
+     * 如果这个数只有下半部分（没有上半部分 / 上半部分为 0），那就直接算
+     */
+    if (tna > tnb)
+    {
+        j = tna - i;
+    }
+    else
+    {
+        j = tnb - i;
+    }
+    if (j == 0)
+    {
+        bn_mul_recursive(&r[n2], &a[n], &b[n], i, tna - i, tnb - i, p);
+        memset(&r[n2 + i * 2], 0, sizeof(BN_TYPE_ULONG) * (n2 - i * 2));
+    }
+    else if (j > 0)
+    {
+        bn_mul_part_recursive(&r[n2], &a[n], &b[n], i, tna - i, tnb - i, p);
+        memset(&r[n2 + tna + tnb], 0, sizeof(BN_TYPE_ULONG) * (n2 - tna - tnb));
+    }
+    else
+    {
+        memset(&r[n2], 0, sizeof(BN_TYPE_ULONG) * n2);
+        if (tna < BN_MUL_RECURSIVE_SIZE_NORMAL &&
+            tnb < BN_MUL_RECURSIVE_SIZE_NORMAL)
+        {
+            bn_mul_normal(&r[n2], &a[n], tna, &b[n], tnb);
+        }
+        else
+        {
+            for (;;)
+            {
+                i /= 2;
+                /*
+                 * 这些简化的判断条件之所以能成立，唯一原因是：
+                 * tna 和 tnb 之间的差值，只可能是 0 或者 1。
+                 */
+                if (i < tna || i < tnb)
+                {
+                    bn_mul_part_recursive(&r[n2], &a[n], &b[n], i, tna - i, tnb - i, p);
+                    break;
+                }
+                else if (i == tna || i == tnb)
+                {
+                    bn_mul_recursive(&r[n2], &a[n], &b[n], i, tna - i, tnb - i, p);
+                    break;
+                }
+
+            }
+        }
+    }
+
+    c1 = (int)bn_add_words(t, r, &r[n2], n2);
+    if (neg)
+    {
+        c1 -= (int)bn_sub_words(&t[n2], t, &t[n2], n2);
+    }
+    else
+    {
+        c1 += (int)bn_add_words(&t[n2], &t[n2], t, n2);
+    }
+
+    c1 += (int)bn_add_words(&r[n], &r[n], &t[n2], n2);
+    if (c1)
+    {
+        p = &(r[n + n2]);
+        lo = *p;
+        ln = (lo + c1) & BN_MASK;
+        *p = ln;
+        /*
+         * 进位传播一定会在 “不该被覆盖的内存” 之前停下来，不会越界写坏数据。
+         */
+        if (ln < (BN_TYPE_ULONG)c1)
+        {
+            do
+            {
+                p++;
+                lo = *p;
+                ln = (lo + 1) & BN_MASK;
+                *p = ln;
+            } while (ln == 0);
+        }
+    }
+>>>>>>> Stashed changes
 }
 
 /**
